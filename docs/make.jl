ba7e38ad27ad5c74443ecbd7c2dac8f8ababb457
gbene <gabri.benedetti@gmail.com>
2026-04-17 10:55:42 +0000

    added examples+changes in readme

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,7 @@
 using Documenter, Squiggles, CairoMakie
 
 pages = ["Home" => "index.md",
+         "Examples" => "examples.md",
          "Code design" => "code.md",
          "Correlograms, coefficients and lags" => "correlate.md",
          "Available kernels" => "kernels.md",
